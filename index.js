a097b8123e280595927e7c599cc33fa4197b7cd0
Shirsho404 <shirsho2402@gmail.com>
2022-04-04 11:48:39 +0530

    Changes

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function mydark()
   document.body.classList.toggle("lightmode");
 
 }
+function icon()
+{
+  if(document.getElementsById("iconchng").src=="icons8-do-not-disturb-ios-24.png")
+  {
+    document.getElementsById("iconchng").src="icons8-sun-50.png";
+  }
+  else{
+    document.getElementsById("iconchng").src="icons8-do-not-disturb-ios-24.png";
+  }
+}
